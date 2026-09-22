486b9b3c92cdcf58537c8678a064cef26499dc9e
unknown <leonangheluta@gamil.com>
2026-09-22 09:39:33 +0200

    Fix bug efficiency class

--- tekworld/spiders/tek_file.py
+++ tekworld/spiders/tek_file.py
@@ -68,11 +68,11 @@ class TekFileSpider(scrapy.Spider):
         price=response.css("div.current-price span::attr(content)").get()
         if not price:
             logging.warning(f"Price not found for {name}")
+        efficiency_class_ok=None
         efficiency_class=response.css("div.tw-energy-copy h2::text").get()
         if efficiency_class:
             efficiency_class_ok=efficiency_class.replace("Classe energetica", "").strip()
         else:
-            efficiency_class_ok=None
             logging.warning(f"Efficiency_class not found for {name}")
         catalogue={"Name": name, "Item_code": item_code, "Price": price, "Category": category, "Efficiency_class": efficiency_class_ok, "Availability": avv}
         details=response.css("div.tw-spec-row")
